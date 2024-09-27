de3fc914874d9a745da4df9307c76aadd9a2d773
PleyDjessy <pleydjessy.09@gmail.com>
2024-09-27 20:57:59 +0200

    Добавлены геттеры для класса "Dog"

--- dogs_git.py
+++ dogs_git.py
@@ -3,3 +3,13 @@ class Dog:
         self.__name = name
         self.__age = age
         self.__color = color
+
+    def get_name(self):
+        return self.__name
+
+    def get_age(self):
+        return self.__age
+
+    def get_color(self):
+        return self.__color
+
